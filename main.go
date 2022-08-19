12b99ba1e2488380487a7ae02f619ec311bc89e8
Erickype <erickype@hotmail.com>
2022-08-19 16:40:55 -0500

    Basic use of template

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"os"
+	"text/template"
+)
 
+func main() {
+	template, err := template.New("greeting").Parse("Hola, mi nombre es {{.}}")
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = template.Execute(os.Stdout, "Erick")
+
+	if err != nil {
+		panic(err)
+	}
 }
