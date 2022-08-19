865be8712a827535838f39812cc90e6263c00a57
Erickype <67160282+Erickype@users.noreply.github.com>
2022-08-19 17:07:56 -0500

    Revert "Templates"

--- main.go
+++ main.go
@@ -1,13 +1,5 @@
 package main
 
-import (
-	"github.com/Erickype/TemplatesGo/models"
-	"github.com/Erickype/TemplatesGo/templates"
-)
-
 func main() {
 
-	person := &models.Person{Nombre: "Erick", Edad: 22}
-
-	templates.LoadTemplate("greetings.txt", person)
 }
