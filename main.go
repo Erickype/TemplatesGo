fbe519b3b4543b0a6e992f6d4c5ab649b4df35a8
Erickype <erickype@hotmail.com>
2022-08-19 16:53:38 -0500

    Use of the person model in the template

--- main.go
+++ main.go
@@ -3,16 +3,21 @@ package main
 import (
 	"os"
 	"text/template"
+
+	"github.com/Erickype/TemplatesGo/models"
 )
 
 func main() {
-	template, err := template.New("greeting").Parse("Hola, mi nombre es {{.}}")
+
+	person := &models.Person{Nombre: "Erick", Edad: 22}
+
+	template, err := template.New("greeting").Parse("Hola, mi nombre es {{.Nombre}} y tengo {{.Edad}} años.")
 
 	if err != nil {
 		panic(err)
 	}
 
-	err = template.Execute(os.Stdout, "Erick")
+	err = template.Execute(os.Stdout, person)
 
 	if err != nil {
 		panic(err)
